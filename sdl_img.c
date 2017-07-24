d39d813274dd4dcd32bb80dbc006dc911f2c3fd4
Robert Winkler <rob121618@gmail.com>
2017-07-23 18:02:49 -0700

    Extended stb_image to handle animated gifs
    per the suggested design (with some minor updates needed)
    
    Now correctly displays animated gifs.  Code still needs lots of cleanup
    and refactoring in prep for doing 4 (or more at a time).

--- sdl_img.c
+++ sdl_img.c
@@ -20,34 +20,34 @@
 
 
 
-void setup(const char* img_name);
-void cleanup(int ret);
-int load_image(const char* path);
-int handle_events();
+typedef struct img_state
+{
+	unsigned char* pixels;
+	int w;
+	int h;
+	int index;
 
-//TODO combine/reorg
-void adjust_rect();
-void set_rect_actual();
-void set_rect_bestfit();
-void set_rect_zoom();
+	int frame_i;
+	int delay; // for now just use the same delay for every frame
+	int frames;
+	int frame_capacity;
+	int frame_timer;
+	
+	SDL_Texture** tex;
 
+	SDL_Rect rect;
+} img_state;
 
 typedef struct global_state
 {
 	SDL_Window* win;
 	SDL_Renderer* ren;
-	SDL_Texture* tex;
-
-	unsigned char* img;
 
 	int scr_w;
 	int scr_h;
 
-	int img_w;
-	int img_h;
-	int img_index;
+	img_state img;
 
-	SDL_Rect rect;
 
 	cvector_str files;
 
@@ -58,6 +58,19 @@ typedef struct global_state
 } global_state;
 
 global_state gs;
+
+
+void setup(const char* img_name);
+void cleanup(int ret);
+int load_image(const char* path);
+int handle_events();
+
+//TODO combine/reorg
+void adjust_rect(img_state* img);
+void set_rect_actual(img_state* img);
+void set_rect_bestfit();
+void set_rect_zoom(img_state* img, int zoom);
+
 
 
 
@@ -175,7 +188,7 @@ int main(int argc, char** argv)
 			perror("stat");
 			continue;
 		}
-		if (S_ISREG(file_stat.st_mode)) {
+		if (S_ISREG(file_stat.st_mode) && stbi_info(fullpath, NULL, NULL, NULL)) {
 			cvec_push_str(&gs.files, fullpath);
 		}
 	}
@@ -184,27 +197,38 @@ int main(int argc, char** argv)
 	qsort(gs.files.a, gs.files.size, sizeof(char*), cmp_string_lt);
 
 	for (int i=0; i<gs.files.size; ++i) {
+		// TODO what is this for again?
 		if (!strcmp(gs.files.a[i], path))
-			gs.img_index = i;
+			gs.img.index = i;
 	}
 
 	set_rect_bestfit();
 
 	int status;
+	int ticks;
 	while (1) {
 		status = handle_events();
 		if (status == QUIT) {
 			break;
 		}
 
-		if (gs.mouse_state && SDL_GetTicks() - gs.mouse_timer > 5000) {
+		ticks = SDL_GetTicks();
+
+		if (gs.mouse_state && ticks - gs.mouse_timer > 5000) {
 			SDL_ShowCursor(SDL_DISABLE);
 			gs.mouse_state = 0;
 		}
 
+		if (gs.img.frames > 1 && ticks - gs.img.frame_timer > gs.img.delay) {
+			gs.img.frame_i = (gs.img.frame_i + 1) % gs.img.frames;
+			gs.img.frame_timer = ticks; // should be set after present ...
+			printf("frame %d %d\n", gs.img.frame_i, gs.img.delay);
+			status = REDRAW;
+		}
+
 		if (status == REDRAW) {
 			SDL_RenderClear(gs.ren);
-			SDL_RenderCopy(gs.ren, gs.tex, NULL, &gs.rect);
+			SDL_RenderCopy(gs.ren, gs.img.tex[gs.img.frame_i], NULL, &gs.img.rect);
 			SDL_RenderPresent(gs.ren);
 		}
 	}
@@ -222,75 +246,98 @@ int main(int argc, char** argv)
 
 
 //need to think about best way to organize following 4 functions' functionality
-void adjust_rect()
+void adjust_rect(img_state* img)
 {
-	gs.rect.x = (gs.scr_w-gs.rect.w)/2;
-	gs.rect.y = (gs.scr_h-gs.rect.h)/2;
+	img->rect.x = (gs.scr_w-img->rect.w)/2;
+	img->rect.y = (gs.scr_h-img->rect.h)/2;
 }
 
 
 void set_rect_bestfit(int fill_screen)
 {
-	float aspect = gs.img_w/(float)gs.img_h;
+	float aspect = gs.img.w/(float)gs.img.h;
 	int h, w;
 	
 	int tmp = MIN(gs.scr_h, gs.scr_w/aspect);
 	if (fill_screen)
 		h = tmp;
 	else
-		h = MIN(tmp, gs.img_h); //show actual size if smaller than viewport
+		h = MIN(tmp, gs.img.h); //show actual size if smaller than viewport
 
 	w = h * aspect;
 
-	gs.rect.x = (gs.scr_w-w)/2;
-	gs.rect.y = (gs.scr_h-h)/2;
-	gs.rect.w = w;
-	gs.rect.h = h;
+	gs.img.rect.x = (gs.scr_w-w)/2;
+	gs.img.rect.y = (gs.scr_h-h)/2;
+	gs.img.rect.w = w;
+	gs.img.rect.h = h;
 }
 
-void set_rect_actual()
+void set_rect_actual(img_state* img)
 {
-	gs.rect.x = (gs.scr_w-gs.img_w)/2;
-	gs.rect.y = (gs.scr_h-gs.img_h)/2;
-	gs.rect.w = gs.img_w;
-	gs.rect.h = gs.img_h;
+	img->rect.x = (gs.scr_w-img->w)/2;
+	img->rect.y = (gs.scr_h-img->h)/2;
+	img->rect.w = img->w;
+	img->rect.h = img->h;
 }
 
-void set_rect_zoom(int zoom)
+void set_rect_zoom(img_state* img, int zoom)
 {
-	float aspect = gs.img_w/(float)gs.img_h;
+	float aspect = img->w/(float)img->h;
 	int h, w;
 	
-	h = gs.rect.h * (1.0 + zoom*0.05);
+	h = img->rect.h * (1.0 + zoom*0.05);
 	w = h * aspect;
 
-	gs.rect.x = (gs.scr_w-w)/2;
-	gs.rect.y = (gs.scr_h-h)/2;
-	gs.rect.w = w;
-	gs.rect.h = h;
+	img->rect.x = (gs.scr_w-w)/2;
+	img->rect.y = (gs.scr_h-h)/2;
+	img->rect.w = w;
+	img->rect.h = h;
 }
 
 
 
 int load_image(const char* path)
 {
-	int n;
-	gs.img = stbi_load(path, &gs.img_w, &gs.img_h, &n, 4);
-	if (!gs.img) {
+	int frames, n;
+
+	//clean up previous image if any
+	stbi_image_free(gs.img.pixels);
+	for (int i=0; i<gs.img.frames; ++i) {
+		SDL_DestroyTexture(gs.img.tex[i]);
+	}
+
+	//gs.img = stbi_load(path, &gs.img_w, &gs.img_h, &n, 4);
+	gs.img.pixels = stbi_xload(path, &gs.img.w, &gs.img.h, &n, 4, &frames);
+	if (!gs.img.pixels) {
 		//printf("failed to load %s: %s\n", path, stbi_failure_reason());
 		return 0;
 	}
 
-	gs.tex = SDL_CreateTexture(gs.ren, SDL_PIXELFORMAT_ABGR8888, SDL_TEXTUREACCESS_STATIC, gs.img_w, gs.img_h);
-	if (!gs.tex) {
-		printf("Error: %s\n", SDL_GetError());
-		return 0;
+
+	if (frames > gs.img.frame_capacity) {
+		gs.img.tex = realloc(gs.img.tex, gs.img.frame_capacity*2*sizeof(SDL_Texture*));
 	}
 
-	if (SDL_UpdateTexture(gs.tex, NULL, gs.img, gs.img_w*4)) {
-		printf("Error updating texture: %s\n", SDL_GetError());
-		return 0;
+	size_t size = gs.img.w * gs.img.h * 4;
+
+	for (int i=0; i<frames; ++i) {
+		gs.img.tex[i] = SDL_CreateTexture(gs.ren, SDL_PIXELFORMAT_ABGR8888, SDL_TEXTUREACCESS_STATIC, gs.img.w, gs.img.h);
+		if (!gs.img.tex[i]) {
+			printf("Error: %s\n", SDL_GetError());
+			return 0;
+		}
+		if (SDL_UpdateTexture(gs.img.tex[i], NULL, gs.img.pixels+(size+2)*i, gs.img.w*4)) {
+			printf("Error updating texture: %s\n", SDL_GetError());
+			return 0;
+		}
 	}
+
+	//gif delay is in 100ths, ticks are 1000ths
+	gs.img.delay = *(short*)(&gs.img.pixels[size]) * 10;
+
+	gs.img.frame_i = 0;
+	gs.img.frames = frames;
+
 
 	return 1;
 }
@@ -299,9 +346,11 @@ void setup(const char* img_name)
 {
 	gs.win = NULL;
 	gs.ren = NULL;
-	gs.tex = NULL;
 
-	gs.img = NULL;
+	memset(&gs.img, 0, sizeof(img_state));
+
+	gs.img.tex = malloc(100*sizeof(SDL_Texture*));
+	gs.img.frame_capacity = 100;
 
 	gs.fullscreen = 0;
 
@@ -366,9 +415,9 @@ int handle_events()
 					return QUIT;
 				}
 
-			case SDL_SCANCODE_1:
+			case SDL_SCANCODE_A:
 				status = REDRAW;
-				set_rect_actual();
+				set_rect_actual(&gs.img);
 				break;
 
 			case SDL_SCANCODE_F: {
@@ -401,14 +450,11 @@ int handle_events()
 			case SDL_SCANCODE_RIGHT:
 			case SDL_SCANCODE_DOWN:
 				status = REDRAW;
-				stbi_image_free(gs.img);
-				SDL_DestroyTexture(gs.tex);
-
 				do {
-					gs.img_index = (gs.img_index + 1) % gs.files.size;
-				} while (!(ret = load_image(gs.files.a[gs.img_index])));
+					gs.img.index = (gs.img.index + 1) % gs.files.size;
+				} while (!(ret = load_image(gs.files.a[gs.img.index])));
 
-				SDL_SetWindowTitle(gs.win, basename(gs.files.a[gs.img_index], title_buf));
+				SDL_SetWindowTitle(gs.win, basename(gs.files.a[gs.img.index], title_buf));
 
 				set_rect_bestfit(gs.fullscreen);
 				break;
@@ -416,14 +462,12 @@ int handle_events()
 			case SDL_SCANCODE_LEFT:
 			case SDL_SCANCODE_UP:
 				status = REDRAW;
-				stbi_image_free(gs.img);
-				SDL_DestroyTexture(gs.tex);
 
 				do {
-					gs.img_index = (gs.img_index-1 < 0) ? gs.files.size-1 : gs.img_index-1;
-				} while (!(ret = load_image(gs.files.a[gs.img_index])));
+					gs.img.index = (gs.img.index-1 < 0) ? gs.files.size-1 : gs.img.index-1;
+				} while (!(ret = load_image(gs.files.a[gs.img.index])));
 
-				SDL_SetWindowTitle(gs.win, basename(gs.files.a[gs.img_index], title_buf));
+				SDL_SetWindowTitle(gs.win, basename(gs.files.a[gs.img.index], title_buf));
 
 				set_rect_bestfit(gs.fullscreen);
 				break;
@@ -446,12 +490,13 @@ int handle_events()
 		case SDL_MOUSEWHEEL:
 			status = REDRAW;
 			if (e.wheel.direction == SDL_MOUSEWHEEL_NORMAL)
-				set_rect_zoom(e.wheel.y);
+				set_rect_zoom(&gs.img, e.wheel.y);
 			else
-				set_rect_zoom(-e.wheel.y);
+				set_rect_zoom(&gs.img, -e.wheel.y);
 			break;
 
 		case SDL_WINDOWEVENT:
+			status = REDRAW;
 			switch (e.window.event) {
 			case SDL_WINDOWEVENT_SIZE_CHANGED:
 				//printf("resized %d x %d\n", e.window.data1, e.window.data2);
@@ -460,7 +505,6 @@ int handle_events()
 
 				set_rect_bestfit(0);
 				//adjust_rect();
-				status = REDRAW;
 
 				break;
 			case SDL_WINDOWEVENT_EXPOSED:
@@ -477,8 +521,11 @@ int handle_events()
 
 void cleanup(int ret)
 {
-	stbi_image_free(gs.img);
-	SDL_DestroyTexture(gs.tex);
+	stbi_image_free(gs.img.pixels);
+
+	for (int i=0; i<gs.img.frames; ++i)
+		SDL_DestroyTexture(gs.img.tex[i]);
+
 	SDL_DestroyRenderer(gs.ren);
 	SDL_DestroyWindow(gs.win);
 
