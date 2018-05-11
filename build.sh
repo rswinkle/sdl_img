de252eef291b5a4d77ad96b9ab68211307e4ba86
Robert Winkler <rob121618@gmail.com>
2018-05-11 13:32:54 -0700

    Second attempt at Travis-CI

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-gcc -std=c99 -g -O0 src/sdl_img.c -o sdl_img `sdl2-config --libs --cflags` `curl-config --libs --cflags` -lm -Wall
+${CC:=gcc} -std=c99 -g -O0 src/sdl_img.c -o sdl_img `sdl2-config --libs --cflags` `curl-config --libs --cflags` -lm -Wall
--- build_release.sh
+++ build_release.sh
@@ -1,1 +1,1 @@
-gcc -std=c99 -O2 src/sdl_img.c -o sdl_img -DNDEBUG `sdl2-config --libs --cflags` `curl-config --libs --cflags` -lm
+${CC:=gcc} -std=c99 -O2 src/sdl_img.c -o sdl_img -DNDEBUG `sdl2-config --libs --cflags` `curl-config --libs --cflags` -lm
