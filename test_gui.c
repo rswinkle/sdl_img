bb8776da9a393f65d49edce7e29787e9b573eb88
Robert Winkler <rob121618@gmail.com>
2019-06-01 17:04:00 -0700

    Working on an about popup

--- test_gui.c
+++ test_gui.c
@@ -24,6 +24,27 @@ int n_imgs = 1;
 SDL_Window* win;
 SDL_Renderer* ren;
 int running;
+int slideshow = nk_false;
+int show_about = nk_false;
+
+char license[1024] =
+"The MIT License (MIT)\n"
+"\n"
+"Copyright (c) 2017-2019 Robert Winkler\n"
+"\n"
+"Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated\n"
+"documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation\n"
+"the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and\n"
+"to permit persons to whom the Software is furnished to do so, subject to the following conditions:\n"
+"\n"
+"The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\n"
+"\n"
+"THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED\n"
+"TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL\n"
+"THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF\n"
+"CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS\n"
+"IN THE SOFTWARE.";
+
 
 void draw_gui(struct nk_context* ctx);
 
@@ -45,10 +66,10 @@ int main(void)
 
 	int win_flags = SDL_WINDOW_SHOWN | SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_RESIZABLE;
 
-	win = SDL_CreateWindow("SDL2_gfx demo",
-                              SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                              WINDOW_WIDTH, WINDOW_HEIGHT,
-                              win_flags);
+	win = SDL_CreateWindow("sdl_img gui",
+	                       SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+	                       WINDOW_WIDTH, WINDOW_HEIGHT,
+	                       win_flags);
 
 	if (!win) {
 		printf("Can't create window: %s\n", SDL_GetError());
@@ -75,8 +96,17 @@ int main(void)
 		SDL_Event evt;
 		nk_input_begin(ctx);
 		while (SDL_PollEvent(&evt)) {
-			if (evt.type == SDL_QUIT || evt.type == SDL_KEYUP && evt.key.keysym.scancode == SDL_SCANCODE_ESCAPE)
+			if (evt.type == SDL_QUIT)
 				goto cleanup;
+
+			if (evt.type == SDL_KEYUP && evt.key.keysym.scancode == SDL_SCANCODE_ESCAPE) {
+				if (show_about)
+					show_about = nk_false;
+				else if (slideshow)
+					slideshow = 0;
+				else
+					goto cleanup;
+			}
 			nk_sdl_handle_event(&evt);
 		}
 		nk_input_end(ctx);
@@ -115,8 +145,7 @@ void draw_gui(struct nk_context* ctx)
 	struct nk_rect bounds;
 	const struct nk_input* in = &ctx->input;
 
-	static int fill = 0;
-	static int slideshow = 0;
+	static int fill = nk_false;
 
 
 	if (nk_begin(ctx, "Menu", nk_rect(0, 0, scr_w, 60), gui_flags)) {
@@ -161,7 +190,9 @@ void draw_gui(struct nk_context* ctx)
 
 		if (nk_menu_begin_label(ctx, "MENU", NK_TEXT_LEFT, nk_vec2(120, 200))) {
 			nk_layout_row_dynamic(ctx, 0, 1);
-			nk_menu_item_label(ctx, "About", NK_TEXT_LEFT);
+			if (nk_menu_item_label(ctx, "About", NK_TEXT_LEFT)) {
+				show_about = nk_true;
+			}
 			nk_menu_item_label(ctx, "Delete", NK_TEXT_LEFT);
 			if (nk_menu_item_label(ctx, "Exit", NK_TEXT_LEFT)) {
 				running = 0;
@@ -220,8 +251,43 @@ void draw_gui(struct nk_context* ctx)
 		if (nk_button_label(ctx, "8")) {
 			n_imgs = 8;
 		}
+
+		if (show_about)
+		{
+			static int license_len;;
+			license_len = strlen(license);
+			int w = 400, h = 400;
+			struct nk_rect s;
+			s.x = scr_w/2-w/2;
+			s.y = scr_h/2-h/2;
+			s.w = w;
+			s.h = h;
+			if (nk_popup_begin(ctx, NK_POPUP_STATIC, "About sdl_img", NK_WINDOW_CLOSABLE, s))
+			{
+				nk_layout_row_dynamic(ctx, 20, 1);
+				nk_label(ctx, "sdl_img 1.0", NK_TEXT_CENTERED);
+				nk_label(ctx, "By Robert Winkler", NK_TEXT_LEFT);
+				nk_label(ctx, "robertwinkler.com", NK_TEXT_LEFT);  //TODO project website
+				nk_label(ctx, "sdl_img is licensed under the MIT License.",  NK_TEXT_LEFT);
+
+				// credits
+				// Sean T Barret (sp?) single header libraries
+				// stb_image, stb_image_write
+				//
+				// nuklear (which also uses stb libs)
+				//
+				// My own cvector lib
+
+				//nk_layout_row_dynamic(ctx, 200, 1);
+				//nk_label_wrap(ctx, license);
+				//nk_edit_string(ctx, NK_EDIT_BOX, license, &license_len, 1024, nk_filter_default);
+				nk_popup_end(ctx);
+			} else show_about = nk_false;
+		}
 	}
+
 	nk_end(ctx);
+
 
 
 	if (nk_begin(ctx, "Controls", nk_rect(0, scr_h-30, scr_w, 30), gui_flags))
