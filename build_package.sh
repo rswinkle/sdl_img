60d9ac70dcca18269d11d965f3d59ccc3996afc5
Robert Winkler <rob121618@gmail.com>
2019-06-30 02:28:45 -0700

    Fix packaging script to depend on libcurl4

--- build_package.sh
+++ build_package.sh
@@ -11,15 +11,17 @@ cp sdl_img.1 ./package_linux/usr/local/share/man/man1
 
 fpm -s dir -t deb -v 0.97 -n sdl_img -C package_linux \
 --log info --verbose \
--d "libsdl2-2.0-0 >= 2.0.5" -d "libsdl2-gfx-1.0-0" -d "libcurl3" \
+-d "libsdl2-2.0-0 >= 2.0.5" -d "libsdl2-gfx-1.0-0" -d "libcurl4" \
 -m "Robert Winkler <rob121618@gmail.com>" \
+--description "A simple image viewer based on SDL2 and stb_image"
 --license MIT \
 --url "https://github.com/rswinkle/sdl_img"
 
 
 fpm -s dir -t tar -v 0.97 -n sdl_img_0.97 -C package_linux \
 --log info --verbose \
--d "libsdl2-2.0-0 >= 2.0.5" -d "libsdl2-gfx-1.0-0" -d "libcurl3" \
+-d "libsdl2-2.0-0 >= 2.0.5" -d "libsdl2-gfx-1.0-0" -d "libcurl4" \
 -m "Robert Winkler <rob121618@gmail.com>" \
+--description "A simple image viewer based on SDL2 and stb_image"
 --license MIT \
 --url "https://github.com/rswinkle/sdl_img"
