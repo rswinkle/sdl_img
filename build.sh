beb18f7a603033ab400977e2a59c7b2572e592fd
Robert Winkler <rob121618@gmail.com>
2023-03-17 00:19:45 -0700

    Ignore some things, -fsanitize=undefined

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-${CC:=gcc} -fsanitize=address -std=gnu99 -g -O0 src/sdl_img.c -o sdl_img `sdl2-config --libs --cflags` `curl-config --libs --cflags` -lSDL2_gfx -lm -Wall
+${CC:=gcc} -fsanitize=address -fsanitize=undefined -std=gnu99 -g -O0 src/sdl_img.c -o sdl_img `sdl2-config --libs --cflags` `curl-config --libs --cflags` -lSDL2_gfx -lm -Wall
