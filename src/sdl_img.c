99297a45621aef6b8ca0cc69a41dd4b354a9821c
Robert Winkler <rob121618@gmail.com>
2025-01-07 11:11:38 -0700

    Add recents for Linux

--- src/sdl_img.c
+++ src/sdl_img.c
@@ -1953,6 +1953,145 @@ void print_help(char* prog_name, int verbose)
 	}
 }
 
+// TODO maybe I should move these recents related functions to their own file
+char* uri_decode(const char* str)
+{
+	int len = strlen(str);
+	char* dst = malloc(len+1);
+	int x, a, b;
+	char hex[3] = {0};
+	int j = 0;
+	
+	for (int i=0; str[i]; ++i) {
+		if (str[i] == '%') {
+			a = str[i+1];
+			b = str[i+2];
+			if (!a) {
+				// copy %?
+				return dst;
+			}
+			if (isxdigit(a) && isxdigit(b)) {
+				hex[0] = a;
+				hex[1] = b;
+				x = strtol(hex, NULL, 16);
+				dst[j++] = x;
+				i += 2;
+			}
+		} else if (str[i] == '&') {
+			if (!strncmp(str+i+1, "apos;", 5)) {
+				dst[j++] = '\'';
+				i += 5;
+			} else if (!strncmp(str+i+1, "amp;", 4)) {
+				dst[j++] = '&';
+				i += 4;
+			} else {
+				dst[j++] = str[i];
+			}
+		} else {
+			dst[j++] = str[i];
+		}
+	}
+	dst = realloc(dst, j+1);
+	dst[j] = 0;
+
+	return dst;
+}
+
+/** Read file into into allocated string, return in *out
+ * Data is NULL terminated.  file is closed before returning (since you
+ * just read the entire file ...). */
+int file_read(FILE* file, char** out)
+{
+	assert(file);
+	assert(out);
+
+	char* data = NULL;
+	long size;
+
+	fseek(file, 0, SEEK_END);
+	size = ftell(file);
+	if (size <= 0) {
+		if (size == -1)
+			perror("ftell failure");
+		fclose(file);
+		return 0;
+	}
+
+	data = (char*)malloc(size+1);
+	if (!data) {
+		fclose(file);
+		return 0;
+	}
+
+	rewind(file);
+	if (!fread(data, size, 1, file)) {
+		perror("fread failure");
+		fclose(file);
+		free(data);
+		return 0;
+	}
+
+	data[size] = 0; /* null terminate in all cases even if reading binary data */
+
+	*out = data;
+
+	fclose(file);
+	return size;
+}
+
+int linux_recents(cvector_str* recents, void* userdata)
+{
+	assert(recents);
+
+	char path[MAX_PATH_LEN];
+	snprintf(path, MAX_PATH_LEN, "%s/.local/share/recently-used.xbel", get_homedir());
+	FILE* recents_file = fopen(path, "r");
+	if (!recents_file) {
+		perror("Could not open recents file");
+		return 0;
+	}
+
+	// TODO cvector_str* or char***, obviously leaning toward former
+	cvec_reserve_str(recents, 200);
+
+	char* text = NULL;
+	int file_len = 0;
+
+	if (!(file_len = file_read(recents_file, &text))) {
+		// empty file
+		return 0;
+	}
+
+	const char needle[] = "bookmark href=\"file://";
+	int needle_len = strlen(needle);
+
+	char* start_search = text;
+
+	char* result = NULL;
+	char* end;
+	char* dst;
+	int i = 0;
+	while ((result = strstr(start_search, needle))) {
+		result += needle_len;
+		end = strchr(result, '\"');
+
+		*end = 0;
+
+		dst = uri_decode(result);
+		//printf("%d %s\n%d %s\n", i, result, i, dst);
+		cvec_pushm_str(recents, dst);
+
+		// continue search just past the end of last result
+		start_search = end+1;
+		i++;
+	}
+
+	free(text);
+
+	// unnecessary with size included in cvector_str...
+	return i;
+}
+
 void setup(int argc, char** argv)
 {
 	char error_str[STRBUF_SZ] = { 0 };
@@ -2185,9 +2324,9 @@ void setup(int argc, char** argv)
 	SDL_Log("Window Max dimensions: %d %d\n", max_w, max_h);
 
 	// init file browser
-#ifdef GNOME
+#ifndef _WIN32
 	// TODO handle different recents functions for linux/windows
-	init_file_browser(&g->filebrowser, default_exts, NUM_DFLT_EXTS, NULL, gnome_recents, NULL);
+	init_file_browser(&g->filebrowser, default_exts, NUM_DFLT_EXTS, NULL, linux_recents, NULL);
 #else
 	init_file_browser(&g->filebrowser, g->img_exts, g->n_exts, NULL, NULL, NULL);
 #endif
